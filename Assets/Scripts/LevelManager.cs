cd7973637b8a3d8166e9108c934a3f688b009798
Andika Fathin Azfar <deutandev@pm.me>
2021-09-19 22:00:26 +0700

    Add Level Manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private Transform _towerUIParent;
+    [SerializeField] private GameObject _towerUIPrefab;
+    [SerializeField] private Tower[] _towerPrefabs;
+
+    // Singleton function
+    private static LevelManager _instance = null;
+    public static LevelManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<LevelManager> ();
+            }
+
+            return _instance;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InstantiateAllTowerUI ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Show all available Tower in Tower Selection UI
+    private void InstantiateAllTowerUI ()
+    {
+        foreach (Tower tower in _towerPrefabs)
+        {
+            GameObject newTowerUIObj = Instantiate (_towerUIPrefab.gameObject, _towerUIParent);
+            TowerUI newTowerUI = newTowerUIObj.GetComponent<TowerUI> ();
+
+            newTowerUI.SetTowerPrefab (tower);
+            newTowerUI.transform.name = tower.name;
+        }
+    }
+}
